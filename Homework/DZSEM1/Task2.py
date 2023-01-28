e61568bce8cbace482329d13fde0c8f83eefa084
KrohmalAV <Anton.avk@yandex.ru>
2023-01-29 00:48:41 +0300

    Задача 2: проверка истинности выражения - оптимизирован вывод

--- Homework/DZSEM1/Task2.py
+++ Homework/DZSEM1/Task2.py
@@ -8,7 +8,7 @@ for x in [0, 1]:
         for z in [0, 1]:
             res1 = not(x or y or z)
             res2 = not(x) and not(y) and not(z)
-            print("При X =", bool(x), "Y =", bool(y), "и Z =", bool(y), "истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z)  => ", res1==res2) 
+            print(f"При X = {bool(x)} Y = {bool(y)} и Z = {bool(y)} истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z)  => {res1==res2}") 
             if res1 == res2:
                 count += 1
 if count == 2**3:
